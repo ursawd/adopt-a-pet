320d28fae3671e9fac24abe2338e3fb38412ffc0
Phil Bailey <philbailey@msn.com>
2021-02-28 19:12:31 -0500

    added login route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template, flash
+from flask import Flask, request, redirect, render_template, flash, session
 from models import db, connect_db, User, Pet
 import requests, json
 from libs.petlib import get_API_response
@@ -21,3 +21,28 @@ app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
 @app.route("/")
 def home():
     return render_template("home.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def user_login():
+    """ Login user """
+
+    # create form
+    form = LoginForm()
+    # check if valid  submitted, this check false if GET
+    if form.validate_on_submit():
+        # get form values
+        username = form.username.data
+        password = form.password.data
+        # check username and hashed password to see if in db
+        user = User.login(username, password)
+        # if username and password match, store username, see to protected page(s)
+        if user:  # contains found user object or False
+            # set session data key "username" to contain logged in username
+            session["username"] = user.username
+            return redirect("/")
+        else:
+            # inform user of bad input
+            form.username.errors = ["Invalid username / password"]
+    # here if either GET route or bad input to login form
+    return render_template("login.html", form=form)
