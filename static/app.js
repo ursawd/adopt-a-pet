d4d04590f15f306154a04efba92133dbb9c66643
Phil Bailey <philbailey@msn.com>
2021-03-05 11:04:18 -0500

    add responsive to displaypets.html

--- static/app.js
+++ static/app.js
@@ -1,10 +1,13 @@
+//###############################################################
+// Listens for user to click on Save Note link on each pet card
+//
 $(".note-link").click(function (e) {
-    // get the element clicked
+    // get the a tag element clicked
     let el = e.target;
     // get the id data attribute from the element
     let apiid = el.dataset.id;
     // use the retrieved id to get the value of the text area
     note = $(`#${apiid}`).val();
-
     console.log(note);
 });
+//###############################################################
